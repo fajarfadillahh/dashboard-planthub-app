10b3e6bbe99f77aeb59f6a47cadbf57d275732cc
gufronnakaaw <gufronnakaaw@gmail.com>
2024-03-23 21:49:47 +0700

    add SessionProvider in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import "@/styles/globals.css";
 
+import { NextUIProvider } from "@nextui-org/react";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
-import {NextUIProvider} from '@nextui-org/react'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
-  )
+    <SessionProvider session={session}>
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </SessionProvider>
+  );
 }
