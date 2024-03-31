731a4330e613d87fdd5d55f88d7adde595e531dc
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2024-03-31 15:46:33 +0700

    update order payment-failed & paymeny-success page

--- pages/order/payment-success/index.tsx
+++ pages/order/payment-success/index.tsx
@@ -1,13 +1,97 @@
 import React from "react";
 import Head from "next/head";
-import { Input } from "@nextui-org/react";
-import { MagnifyingGlass } from "@phosphor-icons/react";
+import {
+  Button,
+  Input,
+  SortDescriptor,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@nextui-org/react";
+import { Eye, MagnifyingGlass } from "@phosphor-icons/react";
 
 // components
 import Layout from "@/components/wrapper/layout";
 import Container from "@/components/wrapper/container";
 
+// utils
+import { formatRupiah } from "@/utils/formatRupiah";
+
+// dummy data
+import { orders } from "@/_dummy/orders";
+
+type OrderTableType = (typeof orders)[0];
+
+const columns = [
+  { name: "ID Pemesanan", uid: "id", sortable: false },
+  { name: "Pesanan Dari", uid: "order_from", sortable: true },
+  { name: "Waktu Pemesanan", uid: "order_time", sortable: true },
+  { name: "Total Bayar", uid: "total_payment", sortable: true },
+  { name: "Aksi", uid: "action", sortable: false },
+];
+
 export default function PaymentSuccessPage() {
+  const [sortDescriptor, setSortDescriptor] = React.useState<SortDescriptor>({
+    column: "order_time",
+    direction: "descending",
+  });
+
+  const sortedItems = [...orders].sort(
+    (a: OrderTableType, b: OrderTableType) => {
+      const first = a[sortDescriptor.column as keyof OrderTableType] as number;
+      const second = b[sortDescriptor.column as keyof OrderTableType] as number;
+      const cmp = first < second ? -1 : first > second ? 1 : 0;
+
+      return sortDescriptor.direction === "descending" ? -cmp : cmp;
+    },
+  );
+
+  const renderCell = (order: OrderTableType, columnKey: React.Key) => {
+    const cellValue = order[columnKey as keyof OrderTableType];
+
+    switch (columnKey) {
+      case "id":
+        return (
+          <div className="text-sm font-medium text-gray-600">{order.id}</div>
+        );
+      case "order_from":
+        return (
+          <div className="text-sm font-medium text-gray-600">
+            {order.order_from}
+          </div>
+        );
+      case "order_time":
+        return (
+          <div className="text-sm font-medium text-gray-600">
+            {order.order_time}
+          </div>
+        );
+      case "total_payment":
+        return (
+          <div className="text-sm font-medium text-gray-600">
+            Rp{formatRupiah(order.total_payment)}
+          </div>
+        );
+      case "action":
+        return (
+          <Button
+            variant="light"
+            color="success"
+            startContent={<Eye weight="bold" size={16} />}
+            className="font-medium"
+          >
+            Detail
+          </Button>
+        );
+
+      default:
+        return cellValue;
+    }
+  };
+
   return (
     <>
       <Head>
@@ -43,12 +127,44 @@ export default function PaymentSuccessPage() {
               />
 
               <p className="text-sm font-medium text-gray-600">
-                Total: <span className="font-semibold text-green-600">3</span>{" "}
+                Total:{" "}
+                <span className="font-semibold text-green-600">
+                  {orders.length}
+                </span>{" "}
                 pesanan
               </p>
             </div>
 
-            <div>table here</div>
+            <Table
+              isHeaderSticky
+              removeWrapper
+              aria-label="table category"
+              selectionMode="single"
+              sortDescriptor={sortDescriptor}
+              onSortChange={setSortDescriptor}
+              classNames={{
+                base: "max-h-[calc(100vh-100px)] overflow-scroll",
+                table: "min-w-[700px]",
+              }}
+            >
+              <TableHeader columns={columns}>
+                {(column) => (
+                  <TableColumn key={column.uid} allowsSorting={column.sortable}>
+                    {column.name}
+                  </TableColumn>
+                )}
+              </TableHeader>
+
+              <TableBody items={sortedItems}>
+                {(order) => (
+                  <TableRow key={order.id}>
+                    {(columnKey) => (
+                      <TableCell>{renderCell(order, columnKey)}</TableCell>
+                    )}
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
           </div>
         </Container>
       </Layout>
